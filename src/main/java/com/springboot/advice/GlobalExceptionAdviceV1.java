af14c9f8b1ade6b72ffa447c99765bf3196508e8
hskim05150 <sweetsea077@naver.com>
2024-06-13 14:25:52 +0900

    commit

--- src/main/java/com/springboot/advice/GlobalExceptionAdviceV1.java
+++ src/main/java/com/springboot/advice/GlobalExceptionAdviceV1.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 /**
  * MethodArgumentNotValidException 처리
- *  - ErrorResponse를 만드는 로직이 포함되어 있다. (생성자 방식)
+ *  - ㅇㅇErrorResponse를 만드는 로직이 포함되어 있다. (생성자 방식)
  * ConstraintViolationException 미처리
  */
 //@RestControllerAdvice
